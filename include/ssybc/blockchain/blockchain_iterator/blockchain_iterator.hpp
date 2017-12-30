f0b304c0ea6e3ea50f457ecf6083199c0a47619e
Shuyang Sun <sunbuffett@gmail.com>
2017-12-29 20:32:04 -0500

    Add files for blockchain iterator.

---
+++ include/ssybc/blockchain/blockchain_iterator/blockchain_iterator.hpp
@@ -0,0 +1,62 @@
+/**********************************************************************************************************************
+ *
+ * Copyright (c) 2017-2018 Shuyang Sun
+ *
+ * License: MIT
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of
+ * the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ *********************************************************************************************************************/
+
+#ifndef SSYBC_INCLUDE_SSYBC_BLOCKCHAIN_BLOCKCHAIN_ITERATOR_BLOCKCHAIN_ITERATOR_HPP_
+#define SSYBC_INCLUDE_SSYBC_BLOCKCHAIN_BLOCKCHAIN_ITERATOR_BLOCKCHAIN_ITERATOR_HPP_
+
+#include "include/ssybc/general/general.hpp"
+#include "include/ssybc/block/block.hpp"
+#include "include/ssybc/validator/block_validator_less_hash.hpp"
+#include "include/ssybc/miner/block_miner_cpu_brute_force.hpp"
+
+#include <unordered_map>
+#include <string>
+#include <memory>
+
+namespace ssybc {
+
+
+
+
+// -------------------------------------------------- Type Definition -------------------------------------------------
+
+  
+
+// --------------------------------------------- Constructor & Destructor ---------------------------------------------
+
+    
+
+// --------------------------------------------------- Public Method --------------------------------------------------
+
+    
+
+// -------------------------------------------------- Private Member --------------------------------------------------
+
+  
+
+
+}  // namespace ssybc
+
+
+#include "src/blockchain/blockchain_iterator/blockchain_iterator_impl.hpp"
+
+
+#endif  // SSYBC_INCLUDE_SSYBC_BLOCKCHAIN_BLOCKCHAIN_ITERATOR_BLOCKCHAIN_ITERATOR_HPP_
+
